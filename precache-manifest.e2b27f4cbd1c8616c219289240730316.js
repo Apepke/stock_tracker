6d840e36064aee54886cd2937b5e385cb6843216
Alex <alexpepke@Alexs-MacBook-Pro.local>
2019-09-09 18:11:44 -0500

    Updates

---
+++ precache-manifest.e2b27f4cbd1c8616c219289240730316.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054ddf444c3e309fc407d5a48707e5a6",
+    "url": "/stock_tracker/index.html"
+  },
+  {
+    "revision": "999c3ca8ef63f5fb94b0",
+    "url": "/stock_tracker/static/css/2.155b05ca.chunk.css"
+  },
+  {
+    "revision": "5dc671b69a1d1c76c70f",
+    "url": "/stock_tracker/static/css/main.eaf7cbdc.chunk.css"
+  },
+  {
+    "revision": "999c3ca8ef63f5fb94b0",
+    "url": "/stock_tracker/static/js/2.bcdb29e6.chunk.js"
+  },
+  {
+    "revision": "5dc671b69a1d1c76c70f",
+    "url": "/stock_tracker/static/js/main.67dd2c76.chunk.js"
+  },
+  {
+    "revision": "7b60d4146ff16eb3c302",
+    "url": "/stock_tracker/static/js/runtime~main.3626fe2c.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/stock_tracker/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/stock_tracker/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/stock_tracker/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/stock_tracker/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/stock_tracker/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "86bb1031fafcfd67d5f45237bcf6aecf",
+    "url": "/stock_tracker/static/media/stocks.86bb1031.jpeg"
+  }
+]);
